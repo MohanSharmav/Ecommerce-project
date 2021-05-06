cab1fe4ff5a6a0417133419c903f918cab07d635
MohanSharmav <catchmohashama@gmail.com>
2021-05-06 12:22:46 +0530

    redux 1 done

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -58,18 +58,52 @@ import {
   PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
 
+// export const productListReducer = (state = { products: [] }, action) => {
+//   switch (action.type) {
+//     case PRODUCT_LIST_REQUEST:
+//       return { loading: true, products: [] }
+//     case PRODUCT_LIST_SUCCESS:
+//       return { loading: false, products: action.payload }
+//     case PRODUCT_LIST_FAIL:
+//       return { loading: false, error: action.payload }
+//     default:
+//       return state
+//   }
+// }
+
+// export const productDetailsReducer = (
+//   state = { product: { reviews: [] } },
+//   action
+// ) => {
+//   switch (action.type) {
+//     case PRODUCT_DETAILS_REQUEST:
+//       return { loading: true, ...state }
+//     case PRODUCT_DETAILS_SUCCESS:
+//       return { loading: false, product: action.payload }
+//     case PRODUCT_DETAILS_FAIL:
+//       return { loading: false, error: action.payload }
+//     default:
+//       return state
+//   }
+// }
+
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return { loading: true, products: [] }
+      return { loading: true };
     case PRODUCT_LIST_SUCCESS:
-      return { loading: false, products: action.payload }
+      return {
+        loading: false,
+        products: action.payload.products,
+        pages: action.payload.pages,
+        page: action.payload.page,
+      };
     case PRODUCT_LIST_FAIL:
-      return { loading: false, error: action.payload }
+      return { loading: false, error: action.payload };
     default:
-      return state
+      return state;
   }
-}
+};
 
 export const productDetailsReducer = (
   state = { product: { reviews: [] } },
@@ -77,12 +111,15 @@ export const productDetailsReducer = (
 ) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-      return { loading: true, ...state }
+      return { loading: true, ...state };
     case PRODUCT_DETAILS_SUCCESS:
-      return { loading: false, product: action.payload }
+      return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAIL:
-      return { loading: false, error: action.payload }
+      return { loading: false, error: action.payload };
+    // case PRODUCT_DETAILS_RESET:
+    //   return { product: { reviews: [] } };
+
     default:
-      return state
+      return state;
   }
-}
+};
